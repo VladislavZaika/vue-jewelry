37dfc746d7a9c7ffb5ab731bce3595a68982550b
VladislavZaika <67119243+VladislavZaika@users.noreply.github.com>
2024-10-17 18:26:56 +0300

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/jewelry-logo.png" alt="Logo" class="w-11" />
+      <div>
+        <h2 class="font-bold text-xl uppercase">Jewelry Store</h2>
+        <p class="text-slate-400">Лучший магазин бижютерии</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer">
+        <img src="/cart.svg" alt="Cart" />
+        <b>1205 грн.</b>
+      </li>
+      <li class="flex items-center gap-3 cursor-pointer">
+        <img src="/heart.svg" alt="Favorite" />
+        <span>Избранное</span>
+      </li>
+    </ul>
+  </header>
+</template>
