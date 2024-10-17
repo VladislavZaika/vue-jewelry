a465aef8cb62046b7fdd061e999aed27d1083397
VladislavZaika <67119243+VladislavZaika@users.noreply.github.com>
2024-10-17 20:11:34 +0300

    Update Header.vue
    Adding Adaptability

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,23 @@
 <template>
-  <header class="flex justify-between border-b border-slate-200 px-10 py-8">
-    <div class="flex items-center gap-4">
-      <img src="/jewelry-logo.png" alt="Logo" class="w-11" />
+  <header class="flex flex-col sm:flex-row justify-between items-center border-b border-slate-200 px-6 sm:px-10 py-4 sm:py-8">
+    <!-- Логотип и информация -->
+    <div class="flex items-center gap-4 mb-4 sm:mb-0">
+      <img src="/jewelry-logo.png" alt="Logo" class="w-9 sm:w-11" />
       <div>
-        <h2 class="font-bold text-xl uppercase">Jewelry Store</h2>
-        <p class="text-slate-400">Лучший магазин бижютерии</p>
+        <h2 class="font-bold text-lg sm:text-xl uppercase">Jewelry Store</h2>
+        <p class="text-slate-400 text-sm sm:text-base">Лучший магазин бижютерии</p>
       </div>
     </div>
 
-    <ul class="flex items-center gap-10">
+    <!-- Меню -->
+    <ul class="flex flex-col sm:flex-row items-center gap-4 sm:gap-10">
       <li class="flex items-center gap-3 cursor-pointer">
-        <img src="/cart.svg" alt="Cart" />
-        <b>1205 грн.</b>
+        <img src="/cart.svg" alt="Cart" class="w-5 sm:w-auto" />
+        <b class="text-sm sm:text-base">1205 грн.</b>
       </li>
       <li class="flex items-center gap-3 cursor-pointer">
-        <img src="/heart.svg" alt="Favorite" />
-        <span>Избранное</span>
+        <img src="/heart.svg" alt="Favorite" class="w-5 sm:w-auto" />
+        <span class="text-sm sm:text-base">Избранное</span>
       </li>
     </ul>
   </header>
