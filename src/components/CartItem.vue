96137af79eb271ce2e5661b130186b2ca8a41ab6
VladislavZaika <67119243+VladislavZaika@users.noreply.github.com>
2024-10-17 20:22:45 +0300

    Update CartItem.vue
    Adding Adaptability

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -8,14 +8,15 @@ defineProps({
 </script>
 
 <template>
-  <div class="relative flex w-full border border-slate-100 rounded-xl p-4 gap-4">
-    <img :src="img" class="w-16 h-16" alt="Sneaker" />
+  <div class="relative flex w-full border border-slate-100 rounded-xl p-2 sm:p-4 gap-2 sm:gap-4">
+    <img :src="img" class="w-12 h-12 sm:w-16 sm:h-16 object-cover" alt="Sneaker" />
     <div class="flex flex-col w-full">
-      <p>{{ title }}</p>
-      <div class="flex justify-between mt-5">
-        <span class="font-bold">{{ price }} грн.</span>
+      <p class="text-sm sm:text-base lg:text-lg">{{ title }}</p>
+      <div class="flex justify-between mt-2 sm:mt-5">
+        <span class="font-bold text-sm sm:text-base lg:text-lg">{{ price }} грн.</span>
         <img
-          class="cursor-pointer opacity-30 hover:opacity-100 transition"
+          @click="onClickDelete"
+          class="cursor-pointer opacity-30 hover:opacity-100 transition w-4 sm:w-5 lg:w-6"
           src="/close.svg"
           alt="Close"
         />
