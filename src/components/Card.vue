5928ae14a59a9aa98cf292e44bc46a260e3e16ed
VladislavZaika <67119243+VladislavZaika@users.noreply.github.com>
2024-10-19 01:11:05 +0300

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -19,7 +19,7 @@ const onClick = () => {
     class="relative flex flex-col w-full border border-slate-100 rounded-xl p-4 sm:p-6 lg:p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
   >
     <div @click="onClick" class="absolute top-4 sm:top-6 left-4 sm:left-6">
-      <img :src="isLiked ? '/like-2.svg' : '/like-1.svg'" alt="Favorite" class="w-5 sm:w-6 lg:w-8" />
+      <img :src="isLiked ? '/like-2.svg' : '/like-1.svg'" alt="Favorite"  />
     </div>
     <img :src="img" class="w-full h-auto object-cover" alt="Sneaker" />
     <p class="text-sm sm:text-base lg:text-lg mt-4">{{ title }}</p>
@@ -28,7 +28,7 @@ const onClick = () => {
         <span class="text-slate-200 text-xs sm:text-sm">Цена:</span>
         <span class="font-bold text-sm sm:text-base lg:text-lg">{{ price }} грн.</span>
       </div>
-      <img src="/plus.svg" alt="Plus" class="w-5 sm:w-6 lg:w-8" />
+      <img src="/plus.svg" alt="Plus" />
     </div>
   </div>
 </template>
